41bd279c59658d1f50d40357070b1cfdbd4b5219
Matt Harrison <matthewharrison@gmail.com>
2018-03-22 10:27:54 -0600

    slides

--- block/test/test_block.py
+++ block/test/test_block.py
@@ -7,14 +7,27 @@ import block as bl
 import pytest
 
 
-def test_hash():
+@pytest.fixture
+def matt_node():
     node = bl.Node('matt')
+    return node
+
+def test_hash(matt_node):
+    node = matt_node
     gb, hash = node.process_txns([])
     print(f'Genesis block {gb.todict()}, \nHash {hash}')
     assert hash.startswith('0')
 
 
-def test_bad_difficulty():
-    node = bl.Node('matt')
+def test_bad_difficulty(matt_node):
+    #node = bl.Node('matt')
+    node = matt_node
     with pytest.raises(TypeError):
         gb, hash = node.process_txns([], difficulty='1')
+
+@pytest.mark.parametrize('uuid',
+                         ['123', 'matt'])
+def test_node(uuid):
+    #for uuid in ['123', 'matt']:
+    n = bl.Node(uuid)
+    assert n.uuid == uuid
--- skilift/test/test_skilift.py
+++ skilift/test/test_skilift.py
@@ -1,14 +1,38 @@
 import skilift as sk
 
-def test_line_take():
-    line = sk.Line(5)
-    res = line.take(7)
-    assert res == 5
-    assert line.num_people == 0
+import pytest
 
-def test_lift_one_bench():
+
+@pytest.fixture
+def line_5():
     line = sk.Line(5)
+    return line
+
+@pytest.fixture
+def quad_10():
     lift = sk.Lift(10, sk.Quad)
-    res = lift.one_bench(line)
+    return lift
+
+def test_line_take(line_5):
+    res = line_5.take(7)
+    assert res == 5 
+    assert line_5.num_people == 0
+
+def test_lift_one_bench(line_5, quad_10):
+    #lift = sk.Lift(10, sk.Quad)
+    res = quad_10.one_bench(line_5)
     assert res == {'loaded': 4, 'num_benches': 1, 'unloaded': 0}
   
+
+@pytest.mark.parametrize('size', [[], None, '10'])
+def test_lines_bad(size):
+    with pytest.raises(TypeError):
+        line = sk.Line(size)
+        line.take(1)
+
+@pytest.mark.parametrize('size,amt,answer',
+    [(0, 5, 0), (5, 2, 3), (10, 0, 10)])
+def test_line_sizes(size, amt, answer):
+    line = sk.Line(size)
+    res = line.take(amt)
+    assert line.num_people == answer
