4859a4f26459ec61740ea172bb7064983399a116
Matt Harrison <matthewharrison@gmail.com>
2018-03-22 10:43:33 -0600

    update tests

--- skilift/test/test_skilift.py
+++ skilift/test/test_skilift.py
@@ -66,3 +66,20 @@ def test_line_sizes(size, amt, answer):
     line = sk.Line(size)
     res = line.take(amt)
     assert line.num_people == answer
+
+
+def test_half_take(monkeypatch):
+    def half_take(self, amount):
+        amount = int(amount/2)
+        if amount > self.num_people:
+            amount = self.num_people
+        self.num_people -= amount
+        return amount
+
+    monkeypatch.setattr(sk.Line, 'take', half_take)
+    line = sk.Line(10)
+    res = line.take(5)
+    assert res == 2
+    assert line.num_people == 8
+
+    
